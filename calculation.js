40fc089f9e119fb8af89c7ba9fba83363481a106
Kuncoro Lukito <kuncorolukito36@gmail.com>
2023-12-02 17:54:08 +0700

    add transition to calculation

--- calculation.js
+++ calculation.js
@@ -1,3 +1,36 @@
+const transition = {
+    "A":{  //state tujuan, write, arah
+        "a":["B", "$", "R"],
+        "b":["C", "$", "R"],
+        "-":"halt"
+    },
+    "B":{ 
+        "a":["B", "a", "R"],
+        "b":["C", "a", "R"],
+        "-":["D", "a", "L"]
+    },
+    "C":{ 
+        "a":["B", "b", "R"],
+        "b":["C", "b", "R"],
+        "-":["D", "b", "L"]
+    },
+    "D":{ 
+        "a":["D", "a", "L"],
+        "b":["D", "b", "L"],
+        "$":["E", "$", "R"]
+    },
+    "E":{ 
+        "a":[],
+        "b":[],
+        "-":[]
+    },
+    "F":{ 
+        "a":[],
+        "b":[],
+        "-":[]
+    }
+};
+
 function calculate(){
     let head = document.getElementById("head");
     let headChild = head.getElementsByTagName("td");
@@ -7,4 +40,5 @@ function calculate(){
     arrow.setAttribute("class", "arrow-down");
 
     headChild[1].appendChild(arrow);
+    headChild[0].innerHTML = " ";
 }
